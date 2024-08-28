04af43e286b614924440a271d07f5ca5646c39ad
Biruk Bizuayehu <brookbizuayehu@gmail.com>
2024-08-29 00:40:58 +0300

    add util functions

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,1 +1,15 @@
-def loadCsv
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+def read_csv(file_path):
+    return pd.read_csv(file_path)
+
+def check_null_values(df):
+    return df.isnull().values.any()
+
+def headline_length(df):
+    df['headline_length'] = df['headline'].apply(lambda x: len(x))
+    headline_stats = df['headline_length'].describe()
+    return headline_stats
