2bde840b72d0bc0a35d2de68bcc5a60a2167d0e4
Biruk Bizuayehu <brookbizuayehu@gmail.com>
2024-08-29 01:14:02 +0300

    add plot function

--- scripts/utils.py
+++ scripts/utils.py
@@ -13,3 +13,20 @@ def headline_length(df):
     df['headline_length'] = df['headline'].apply(lambda x: len(x))
     headline_stats = df['headline_length'].describe()
     return headline_stats
+
+def publishers_by_articles(df, top_n):
+    print(f'Top {top_n} publishers by articles')
+    article_counts = df.groupby('publisher').size()
+    sorted_counts = article_counts.sort_values(ascending=False)
+    return sorted_counts.head(top_n)
+
+def plot_top_publishers(df, top_n):
+    top_publishers = publishers_by_articles(df, top_n)
+    plt.figure(figsize=(10, 6))
+    top_publishers.plot(kind='bar', color='skyblue')
+    plt.title(f'Top {top_n} Publishers by Number of Articles')
+    plt.xlabel('Publishers')
+    plt.ylabel('Number of Articles')
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+    plt.show()
