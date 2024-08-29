7759efccabe6d4240a6072cd6fd79a0b246c74b9
Biruk Bizuayehu <brookbizuayehu@gmail.com>
2024-08-29 18:36:25 +0300

    add plotting function

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,6 +1,8 @@
 import pandas as pd
+from pandas.tseries.offsets import MonthEnd
 import matplotlib.pyplot as plt
-
+import  matplotlib.dates as mdates
+import  matplotlib.dates as mdates
 
 
 def read_csv(file_path):
@@ -30,3 +32,68 @@ def plot_top_publishers(df, top_n):
     plt.xticks(rotation=45, ha='right')
     plt.tight_layout()
     plt.show()
+
+def publication_dates(df):
+    # Ensure 'date' is in datetime format
+    df['date'] = pd.to_datetime(df['date'],format="ISO8601")
+
+    # Group by date and count articles
+    daily_counts = df.groupby(df['date'].dt.date).size()
+    
+    # Find days with highest article counts
+    top_days = daily_counts.nlargest(5)
+    
+    # Analyze weekday distribution
+    weekday_counts = df['date'].dt.day_name().value_counts()
+    
+    # Monthly trend
+    df['month_start'] = df['date'].dt.floor('D') + MonthEnd(0) - MonthEnd(1)
+
+    monthly_counts = df.groupby(pd.Grouper(key='date', freq='ME')).size()
+
+    
+    return {
+        'daily_counts': daily_counts,
+        'top_days': top_days,
+        'weekday_counts': weekday_counts,
+        'monthly_counts': monthly_counts
+    }
+
+#Plot the publication trends
+def plot_publication_trends(date_analysis):
+
+    fig, axes = plt.subplots(2, 2, figsize=(20, 15))
+    
+    # Daily trend
+    date_analysis['daily_counts'].plot(ax=axes[0, 0])
+    axes[0, 0].set_title('Daily Article Count')
+    axes[0, 0].set_xlabel('Date')
+    axes[0, 0].set_ylabel('Number of Articles')
+    
+    # Top days
+    date_analysis['top_days'].plot(kind='bar', ax=axes[0, 1])
+    axes[0, 1].set_title('Top 5 Days with Most Articles')
+    axes[0, 1].set_xlabel('Date')
+    axes[0, 1].set_ylabel('Number of Articles')
+    
+    # Weekday distribution
+    date_analysis['weekday_counts'].plot(kind='bar', ax=axes[1, 0])
+    axes[1, 0].set_title('Article Distribution by Weekday')
+    axes[1, 0].set_xlabel('Weekday')
+    axes[1, 0].set_ylabel('Number of Articles')
+    
+    # Monthly trend
+    monthly_counts = date_analysis['monthly_counts']
+    monthly_counts.plot(ax=axes[1, 1])
+    axes[1, 1].set_title('Monthly Article Count')
+    axes[1, 1].set_xlabel('Month')
+    axes[1, 1].set_ylabel('Number of Articles')
+    
+    # Format x-axis to show months
+    axes[1, 1].xaxis.set_major_locator(mdates.AutoDateLocator())
+    axes[1, 1].xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+    plt.setp(axes[1, 1].xaxis.get_majorticklabels(), rotation=45, ha='right')
+    
+    
+    plt.tight_layout()
+    return fig
